f3f0968b35e52870c4eddf1da74ba3ec93829470
contentrajabot <109162597+contentrajabot@users.noreply.github.com>
2022-12-14 15:52:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import  os
 
-TOKEN = os.environ.get("TOKEN")
-APP_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-CHAT_ID = os.environ.get("CHAT_ID")
+TOKEN = os.environ.get("5851460987:AAGKXisw48cKwP8yT1Ij5t-WZfZJqAHNnfo")
+APP_ID = int(os.environ.get("818663"))
+API_HASH = os.environ.get("b31cf04c3cc21aa600eca3a991085702")
+CHAT_ID = os.environ.get("-1001414596452")
 
 
 youtube_next_fetch = 0  # time in minute
