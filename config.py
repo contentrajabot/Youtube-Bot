5cd6201f8d622a3c2bf5b1a1f5b73e37c840f3a8
contentrajabot <109162597+contentrajabot@users.noreply.github.com>
2022-12-14 16:03:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import  os
 
 TOKEN = os.environ.get("5851460987:AAGKXisw48cKwP8yT1Ij5t-WZfZJqAHNnfo")
-APP_ID = int(os.environ.get("818663"))
+APP_ID = int(os.environ.get("818663")
 API_HASH = os.environ.get("b31cf04c3cc21aa600eca3a991085702")
 CHAT_ID = os.environ.get("-1001414596452")
 
